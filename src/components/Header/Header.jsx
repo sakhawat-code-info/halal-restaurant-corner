3b4c845a2e91ac75496b93c4b466a0fa501d9877
sakhawat-code-info <sakhawat.code.info@gmail.com>
2024-03-15 19:26:11 +0600

    Header section done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className='container mx-auto'>
+                <div className="navbar bg-base-100">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </div>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a>Home</a></li>
+                                <li><a>Recipes</a></li>
+                                <li><a>About</a></li>
+                                <li><a>Search</a></li>
+                            </ul>
+                        </div>
+                        <a className="btn btn-ghost text-xl">Halal FooD corneR</a>
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            <li><a>Home</a></li>
+                            <li><a>Recipes</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Search</a></li>
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        <div className="form-control">
+                            <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                        </div>
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img alt="Tailwind CSS Navbar component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
